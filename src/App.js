d22e38d8baca1e413f9a6036dd0523ebb89b524d
G00401808@atu.ie <G00401808@atu.ie>
2023-12-10 18:07:49 +0000

    Set up temporary home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="/">Home</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/pokédex">Pokédex</Nav.Link>
+              <Nav.Link href="/encounter">Explore</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          {/* <Route path='/pokédex' element={ }></Route>
+          <Route path='/encounter' element={ }></Route> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
