be4dd4871209ba23c191413c51863e7da5f04375
G00401808@atu.ie <G00401808@atu.ie>
2023-12-11 12:21:02 +0000

    changing functionality to have server process data changes to icon and title

--- backEnd/Server.js
+++ backEnd/Server.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 const port = 4000
 
-
+// Handle GET request for Pokemon data
+app.get('/pokemon', async (req, res) => {
+    const data = await axios.get('https://pokeapi.co/api/v2/pokemon/1')
+        .then((data) => {
+            res.json(data.data)
+        })
+        .catch();
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
