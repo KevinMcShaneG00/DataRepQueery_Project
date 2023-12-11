4f4e420c740639b03e60fd5a91a985357138b981
G00401808@atu.ie <G00401808@atu.ie>
2023-12-11 20:32:48 +0000

    updated code to use session storage for api data to conform to the fair use policy

--- backEnd/Server.js
+++ backEnd/Server.js
@@ -10,11 +10,33 @@ app.use(cors());
 // Handle GET request for Pokemon data
 //note that é = %C3%A9 and is automatically converted in the http req
 app.get('/pok%C3%A9dex', async (req, res) => {
-    const data = await axios.get('https://pokeapi.co/api/v2/pokemon')
-        .then((data) => {
-            res.json(data.data)
+
+    let allPokemon = [];
+    let nextUrl = 'https://pokeapi.co/api/v2/pokemon';
+
+    const fetchPokemonData = () => {
+        return axios.get(nextUrl)
+            .then((response) => {
+                // Appending the fetched Pokémon from response.data.results to the allPokemon array with ... spread syntax
+                allPokemon = [...allPokemon, ...response.data.results];
+                //fetch the next line of data
+                nextUrl = response.data.next;
+
+                //if nextUrl is not null and is therefore a truthy value
+                if (nextUrl) {
+                    return fetchPokemonData(); // Fetch next page recursively
+                }
+            });
+    };
+
+    fetchPokemonData()
+        .then(() => {
+            res.json(allPokemon);
         })
-        .catch();
+        .catch((error) => {
+            console.error('Error fetching Pokémon data:', error);
+            res.status(500).json({ error: 'Failed to fetch Pokémon data' });
+        });
 });
 
 app.listen(port, () => {
--- src/components/Pokédex.js
+++ src/components/Pokédex.js
@@ -3,30 +3,51 @@ import axios from 'axios';
 
 function Pokédex() {
 
+    //declare pokemonList and it's setter as null
     const [pokemonList, setPokemonList] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
-            try {
-                const response = await axios.get('http://localhost:4000/pok%C3%A9dex');
-                setPokemonList(response.data.results);
-            } catch (error) {
-                console.error('Error fetching data:', error);
+            // Check if data is present in sessionStorage
+            const cachedData = sessionStorage.getItem('pokemonData');
+
+            if (cachedData) {
+                console.log("Using data from cache")
+                setPokemonList(JSON.parse(cachedData));
+            }
+
+            else {
+                //get a list of all the pokemon's names and url's
+                const response = await axios.get('http://localhost:4000/pok%C3%A9dex')
+                    .then((response) => {
+                        console.log("making a request from api")
+                        // Store data in sessionStorage for future use
+                        sessionStorage.setItem('pokemonData', JSON.stringify(response.data));
+
+                        setPokemonList(response.data);
+                    })
+                    .catch((error) => {
+                        console.error('Error fetching data:', error);
+                    })
             }
         };
 
+        //call the anonymous function
         fetchData();
     }, []);
 
     return (
         <div>
+            {/* if else statement in javaScript with the operator ? */}
             {pokemonList ? (
+                // run when pokemonList is not null
                 <ul>
                     {pokemonList.map((pokemon, index) => (
                         <li key={index}>{pokemon.name}</li>
                     ))}
                 </ul>
             ) : (
+                // run when it is null
                 <p>Loading...</p>
             )}
         </div>
