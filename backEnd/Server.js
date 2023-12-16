83c72751f9264fa4a6d0a7066516497b4d14b87e
G00401808@atu.ie <G00401808@atu.ie>
2023-12-16 13:11:58 +0000

    /pokédex now fetches images

--- backEnd/Server.js
+++ backEnd/Server.js
@@ -10,31 +10,60 @@ app.use(cors());
 // Handle GET request for Pokemon data
 //note that é = %C3%A9 and is automatically converted in the http req
 app.get('/pok%C3%A9dex', async (req, res) => {
-
     let allPokemon = [];
-    let nextUrl = 'https://pokeapi.co/api/v2/pokemon';
+    //first url in the api
+    let nextUrl = 'https://pokeapi.co/api/v2/pokemon'; 
 
-    const fetchPokemonData = () => {
+    //limit the amount of data requested from the api to prevent 
+    //ECONNRESET error with the api server
+    let count = 0;
+
+    const fetchData = () => {
         return axios.get(nextUrl)
-            .then((response) => {
-                // Appending the fetched Pokémon from response.data.results to the allPokemon array with ... spread syntax
-                allPokemon = [...allPokemon, ...response.data.results];
-                //fetch the next line of data
-                nextUrl = response.data.next;
+        .then((response) => {
+            //to add results to allPokemon, use allPokemon.push, but because .push()expects a single element to be added
+            //we must split up the 20 records in the results array using ...
+            allPokemon.push(...response.data.results);
+            
+            //next url from api
+            nextUrl = response.data.next;
 
-                //if nextUrl is not null and is therefore a truthy value
-                if (nextUrl) {
-                    return fetchPokemonData(); // Fetch next page recursively
-                }
-            });
+            //Update the count
+            count += response.data.results.length; 
+
+            if (nextUrl && count < 400) {
+                //get more if limit has not been reached
+                return fetchData(); 
+            }
+        })
+        .catch((error)=>{
+            console.log("fetchData()" + error);
+        });
     };
 
-    fetchPokemonData()
+    fetchData()
         .then(() => {
-            res.json(allPokemon);
+            //when the fetchData() finishes this happpens
+            //allPokemon.map acts as an iterator for that array
+            const imageRequests = allPokemon.map(async (pokemon) => {
+                //make a request with 
+                const specificPokemonData = await axios.get(pokemon.url);
+
+                //store sprites and return them
+                const sprites = specificPokemonData.data.sprites;
+                return sprites.front_default;
+            });
+
+            // Promise .all details here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
+            // It is essentially used to make the promises return in the order in which they were made
+            return Promise.all(imageRequests);
+        })
+        .then((pokemonImages) => {
+            //when Promise.all() runs it then sends data back to the client
+            res.json({ allPokemon, pokemonImages });
         })
         .catch((error) => {
-            console.error('Error fetching Pokémon data:', error);
+            console.error('Error:', error);
             res.status(500).json({ error: 'Failed to fetch Pokémon data' });
         });
 });
--- src/components/Pokédex.js
+++ src/components/Pokédex.js
@@ -1,56 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Card from 'react-bootstrap/Card';
 
 function Pokédex() {
-
-    //declare pokemonList and it's setter as null
-    const [pokemonList, setPokemonList] = useState(null);
+    const [pokemonData, setPokemonData] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
-            // Check if data is present in sessionStorage
             const cachedData = sessionStorage.getItem('pokemonData');
 
             if (cachedData) {
-                console.log("Using data from cache")
-                setPokemonList(JSON.parse(cachedData));
-            }
-
-            else {
-                //get a list of all the pokemon's names and url's
-                const response = await axios.get('http://localhost:4000/pok%C3%A9dex')
-                    .then((response) => {
-                        console.log("making a request from api")
-                        // Store data in sessionStorage for future use
-                        sessionStorage.setItem('pokemonData', JSON.stringify(response.data));
-
-                        setPokemonList(response.data);
-                    })
-                    .catch((error) => {
-                        console.error('Error fetching data:', error);
-                    })
+                console.log("Using data from cache");
+                setPokemonData(JSON.parse(cachedData));
+            } else {
+                try {
+                    console.log("retrieveing data from server");
+                    const response = await axios.get('http://localhost:4000/pok%C3%A9dex');
+                    sessionStorage.setItem('pokemonData', JSON.stringify(response.data));
+                    setPokemonData(response.data);
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                }
             }
         };
 
-        //call the anonymous function
         fetchData();
     }, []);
 
     return (
         <div>
-            {/* if else statement in javaScript with the operator ? */}
-            {pokemonList ? (
-                // run when pokemonList is not null
-                <ul>
-                    {pokemonList.map((pokemon, index) => (
-                        <li key={index}>{pokemon.name}</li>
+            {pokemonData ? (
+                <div>
+                    {pokemonData.allPokemon.map((pokemon, index) => (
+                        <Card key={index}>
+                            <Card.Header>{pokemon.name}</Card.Header>
+                            <Card.Img variant="top" src={pokemonData.pokemonImages[index]} alt={pokemon.name} />
+                        </Card>
                     ))}
-                </ul>
+                </div>
             ) : (
-                // run when it is null
                 <p>Loading...</p>
             )}
         </div>
     );
 }
+
 export default Pokédex;
