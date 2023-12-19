3c18b99fb9c2b33769453f7d733ea6c8e5c5ec62
G00401808@atu.ie <G00401808@atu.ie>
2023-12-19 10:40:10 +0000

    Encounter.js made code act more predictable so that the random pokemon would only be generated once, stopped useEffect from generating it in rerender

--- src/components/Encounter.js
+++ src/components/Encounter.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Card from 'react-bootstrap/Card';
 
 function Encounter() {
     //generate a random number to pick a pokemon
@@ -14,13 +15,13 @@ function Encounter() {
         let max = 400;
         let randomNumber = Math.round(Math.random() * (max - min) + 1);
         console.log(randomNumber);
-        return randomNumber
+        return randomNumber;
     }
-    let randomID = getRandomForID();
-
     //generate sets and gets for post variables
+    const [pokemon, setPokemon] = useState(null);
     const [pokemonName, setPokemonName] = useState('');
     const [image, setImage] = useState('');
+    const [randomID, setRandomID] = useState('');
 
     //function to add pokemon to the list of pokemon the user owns
     const catchPokemon = () => {
@@ -33,39 +34,52 @@ function Encounter() {
         }
 
         axios.post('http://localhost:4000/catchpokemon', pokeball)//send data with html promise
-            .then()//always need a then and catch hanle for a promise
+            .then(()=>{
+                //tell the user https://www.w3schools.com/js/js_popup.asp
+                alert("pokemon caught successfully");
+            })
             .catch();
     }
 
-    const [pokemon, setPokemon] = useState(null);
-
+    //set counter to ensure that the random and rquest is only made once
+    //every time the page is reloaded or saved it sets counter to 0
+    var useEffectUpdateCounter = 0;
     useEffect(
         () => {
+            if (useEffectUpdateCounter == 0) {
+                var id = getRandomForID();
+                setRandomID(id);
+            
 
-            axios.get('http://localhost:4000/encounter:' + randomID)
-                .then(
-                    (response) => {
-                        setPokemon(response.data);
-                        setPokemonName(response.data.species.name);
-                        setImage(response.data.sprites.front_default);
-                    }
-                )
-                .catch(
-                    (error) => {
-                        console.log(error);
-                    }
-                );
+                console.log('http://localhost:4000/encounter:' + id)
+                axios.get('http://localhost:4000/encounter:' + id)
+                    .then(
+                        (response) => {
+                            //set the data here
+                            setPokemon(response.data);
+                            setPokemonName(response.data.species.name);
+                            setImage(response.data.sprites.front_default);
+                        }
+                    )
+                    .catch(
+                        (error) => {
+                            console.log(error);
+                        }
+                    );
+            }
+            useEffectUpdateCounter++;
         }, []
     );
-
-    console.log(pokemon);
 
     return (
         <div>
             {pokemon ? (
-                <div>
-                    <p>hi {pokemon.species.name}</p>
-                    <button onClick={catchPokemon}>catch</button>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <Card style={{ width: '200px' }}>
+                        <Card.Header>{pokemon.species.name}</Card.Header>
+                        <Card.Img src={pokemon.sprites.front_default} alt={pokemon.species.name} />
+                        <button onClick={catchPokemon}>Catch</button>
+                    </Card>
                 </div>
             ) : (
                 <p>loading</p>
